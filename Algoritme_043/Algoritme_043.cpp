04b1f0626dd89d0c370c612ee999bc1bc5641f77
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-18 21:40:31 +0700

    commit 8

--- Algoritme_043/Algoritme_043.cpp
+++ Algoritme_043/Algoritme_043.cpp
@@ -85,3 +85,20 @@ bool listEmpty() {
 		return false;
 }
 
+void traverse() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << "NIM: " << currentNode->noMhs << ", Nama: " <<
+				currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
