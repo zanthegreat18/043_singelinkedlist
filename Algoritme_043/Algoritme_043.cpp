14441010e299def278b79bea29a5b27ee3666f97
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-18 21:36:38 +0700

    commit2

---
+++ Algoritme_043/Algoritme_043.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+struct Node {
+	int noMhs;
+	string name;
+	Node* next;
+};
+Node* START = NULL; 
+
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM : ";
+	cin >> nim;
+	cout << "Masukan Nama: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return; 
+	}
