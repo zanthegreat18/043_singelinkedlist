e80e95071a1472649176b20f5a43de27a9aa0a76
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-18 21:42:38 +0700

    commit11

--- Algoritme_043/Algoritme_043.cpp
+++ Algoritme_043/Algoritme_043.cpp
@@ -127,4 +127,21 @@ void searchData() {
 	}
 }
 
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+			cout << "1. Tambahan Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan)
+			{
+
+
 
